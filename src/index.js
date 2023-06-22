0bcc2a1e641e1dff48020c829450c5474e1f0571
Dachrono <bluefire_andy@hotmail.com>
2023-06-22 11:24:17 -0600

    First commit and refactor title file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import './style/index.css';
-import title from './modules/title.js';
 import { scoreTitle, list } from './modules/scores.js';
 import form from './modules/form.js';
-
-title();
 
 const box = document.getElementById('container');
 const scoreCont = document.createElement('div');
--- src/modules/title.js
+++
@@ -1,9 +0,0 @@
-const title = () => {
-  const container = document.getElementById('container');
-  const element = document.createElement('h1');
-  element.id = 'title';
-  element.textContent = 'Leaderboard';
-  container.appendChild(element);
-};
-
-export default title;
