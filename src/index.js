676de1ff94e74e3541e72f0cd9746528891f3572
Dachrono <bluefire_andy@hotmail.com>
2023-06-22 00:16:33 -0600

    test3 lighthouse lint error

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import './style/index.css';
-import title from './modules/title.js';
-import { scoreTitle, list } from './modules/scores.js';
-import form from './modules/form.js';
+// import title from './modules/title.js';
+// import { scoreTitle, list } from './modules/scores.js';
+// import form from './modules/form.js';
 
-title();
+// title();
 
-const box = document.getElementById('container');
-const scoreCont = document.createElement('div');
-scoreCont.id = 'scoreCont';
-box.appendChild(scoreCont);
+// const box = document.getElementById('container');
+// const scoreCont = document.createElement('div');
+// scoreCont.id = 'scoreCont';
+// box.appendChild(scoreCont);
 
-scoreTitle();
-list();
-form();
+// scoreTitle();
+// list();
+// form();
